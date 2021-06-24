58e42b4b7f94596938da6aa90129170f5b132755
Svejgo <svejgo@gmail.com>
2021-06-25 01:10:09 +0700

    HW3
    Combine 1 and 3 tasks (did rapid fire as alternate fire :) )

--- Source/Tankogeddon/TankPlayerController.cpp
+++ Source/Tankogeddon/TankPlayerController.cpp
@@ -3,6 +3,12 @@
 
 #include "TankPlayerController.h"
 #include "TankPawn.h"
+#include "DrawDebugHelpers.h"
+
+ATankPlayerController::ATankPlayerController()
+{
+	bShowMouseCursor = true;
+}
 
 void ATankPlayerController::BeginPlay()
 {
@@ -17,6 +23,35 @@ void ATankPlayerController::SetupInputComponent()
 
 	InputComponent->BindAxis("MoveForward", this, &ATankPlayerController::MoveForward);
 	InputComponent->BindAxis("MoveRight", this, &ATankPlayerController::MoveRight);
+	InputComponent->BindAxis("RotateRight", this, &ATankPlayerController::RotateRight);
+	InputComponent->BindAction("Fire",IE_Pressed, this, &ATankPlayerController::Fire);
+	InputComponent->BindAction("AltFire",IE_Pressed, this, &ATankPlayerController::AltFire);
+}
+
+void ATankPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	FVector MousePosition, MouseDirection;
+
+	DeprojectMousePositionToWorld(MousePosition, MouseDirection);
+	
+	if (TankPawn)
+	{
+		FVector TankPosition = TankPawn->GetActorLocation();
+		CachedMousePos = MousePosition - TankPosition;
+		CachedMousePos.Z = 0;
+		CachedMousePos.Normalize();
+		CachedMousePos = TankPosition + CachedMousePos * 1000.f;
+
+		DrawDebugLine(GetWorld(), TankPosition, CachedMousePos, FColor::Green, false, 0.f, 0, 5.f);
+	}
+		
+}
+
+FVector ATankPlayerController::GetMousePos() const
+{
+	return CachedMousePos;
 }
 
 void ATankPlayerController::MoveForward(float AxisValue)
@@ -32,5 +67,27 @@ void ATankPlayerController::MoveRight(float AxisValue)
 	if (TankPawn)
 	{
 		TankPawn->MoveRight(AxisValue);
+	}
+}
+
+void ATankPlayerController::RotateRight(float AxisValue)
+{
+	if (TankPawn)
+	{
+		TankPawn->RotateRight(AxisValue);
+	}
+}
+
+void ATankPlayerController::Fire()
+{
+	if (TankPawn)
+	{
+		TankPawn->Fire();
+	}
+}void ATankPlayerController::AltFire()
+{
+	if (TankPawn)
+	{
+		TankPawn->AltFire();
 	}
 }

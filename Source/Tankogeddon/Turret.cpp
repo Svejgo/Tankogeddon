a91a9e4d5b1e3dd964d63c6e39cfecf6f2f250b9
Svejgo <svejgo@gmail.com>
2021-07-05 21:24:49 +0700

    HW5 incomplete
    Didn't any of home tasks, haven't time :(

---
+++ Source/Tankogeddon/Turret.cpp
@@ -0,0 +1,108 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Turret.h"
+#include "TankPlayerController.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Cannon.h"
+#include "TimerManager.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+ATurret::ATurret()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret body"));
+	RootComponent = BodyMesh;
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret turret"));
+	TurretMesh->AttachToComponent(BodyMesh, FAttachmentTransformRules::KeepRelativeTransform);
+
+	CannonSetupPoint = CreateDefaultSubobject<UArrowComponent>(TEXT("Cannon setup point"));
+	CannonSetupPoint->AttachToComponent(TurretMesh, FAttachmentTransformRules::KeepRelativeTransform);
+
+	HitCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("Hit collider"));
+	HitCollider->SetupAttachment(TurretMesh);
+
+	UStaticMesh* TurretMeshTemp = LoadObject<UStaticMesh>(this, *TurretMeshPath);
+	if (TurretMeshTemp)
+	{
+		TurretMesh->SetStaticMesh(TurretMeshTemp);
+	}
+
+	UStaticMesh* BodyMeshTemp = LoadObject<UStaticMesh>(this, *BodyMeshPath);
+	if (BodyMeshTemp)
+	{
+		BodyMesh->SetStaticMesh(BodyMeshTemp);
+	}
+}
+
+// Called when the game starts or when spawned
+void ATurret::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FActorSpawnParameters Params;
+	Params.Owner = this;
+	Cannon = GetWorld()->SpawnActor<ACannon>(CannonClass, Params);
+	Cannon->AttachToComponent(CannonSetupPoint, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+
+	PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	FTimerHandle _targetingTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(_targetingTimerHandle, this, &ATurret::Targeting, TargetingRate, true, TargetingRate);
+}
+
+void ATurret::Destroyed()
+{
+	if (Cannon)
+		Cannon->Destroy();
+}
+
+void ATurret::Targeting()
+{
+	if (IsPlayerInRange())
+	{
+		RotateToPlayer();
+	}
+
+	if (CanFire() && Cannon && Cannon->IsReadyToFire())
+	{
+		Fire();
+	}
+}
+
+void ATurret::RotateToPlayer()
+{
+	FRotator TargetRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), PlayerPawn->GetActorLocation());
+	FRotator CurrRotation = TurretMesh->GetComponentRotation();
+	TargetRotation.Pitch = CurrRotation.Pitch;
+	TargetRotation.Roll = CurrRotation.Roll;
+	TurretMesh->SetWorldRotation(FMath::Lerp(CurrRotation, TargetRotation, TargetingSpeed));
+
+
+}
+
+bool ATurret::IsPlayerInRange()
+{
+	return FVector::Distance(PlayerPawn->GetActorLocation(), GetActorLocation()) <= TargetingRange;
+}
+
+bool ATurret::CanFire()
+{
+	FVector TargetingDir = TurretMesh->GetForwardVector();
+	FVector DirToPlayer = PlayerPawn->GetActorLocation() - GetActorLocation();
+	DirToPlayer.Normalize();
+	float AimAngle = FMath::RadiansToDegrees(acosf(FVector::DotProduct(TargetingDir, DirToPlayer)));
+	return AimAngle <= Accurency;
+}
+
+void ATurret::Fire()
+{
+	if (Cannon)
+		Cannon->Fire();
+}
+
